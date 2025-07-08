807e7299cf75fd31a5b9efeef37b9e840260f99f
itsnottakenme <ianmisc@gmail.com>
2025-07-08 13:03:31 -0700

    n.db.  ui.activities  created

--- app/src/main/java/n/db/ui/activities/MainActivity.java
+++ app/src/main/java/n/db/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package n.db;
+package n.db.ui.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import n.db.R;
 
 public class MainActivity extends AppCompatActivity {
 
